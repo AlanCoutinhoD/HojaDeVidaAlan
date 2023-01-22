df742cf6f4ab5bb323fc11ba1ce64c3306a9d13b
AlanCoutinhoD <alanemmanueldiazcoutino@gmail.com>
2023-01-21 22:21:44 -0600

    Actualizar, eliminacion de archivos innecesarios

--- src/components/atoms/Cabecera.jsx
+++ src/components/atoms/Cabecera.jsx
@@ -1,13 +1,13 @@
 import Header from "../../assets/styles/Header.css"
 import computadora from "../../assets/img/pc.png"
-import gato from "../../assets/img/gato.png";
+import Alan from "../../assets/img/Alan.png";
 const Cabecera = () => {
     return ( 
         <> 
         <div class="header">
         <div class="cuadrado"></div>
         <div class="circulo"></div>
-        <img className="imagen" src={gato} ></img>
+        <img className="imagen" src={Alan} ></img>
         <div class="nombre">
             <h1>ALAN EMMANUEL DIAZ COUTIÑO</h1>
         <div className="carrera"><h3>ING. EN SOFTWARE </h3></div>
